d8a4d1547d3e3342ca6c2311a2de6d3d951e743e
Aaron Cho <aaroncho@Aarons-MacBook-Pro.local>
2020-06-21 15:48:20 -0700

    implementing file upload functinality

--- musicbox/src/App.js
+++ musicbox/src/App.js
@@ -1,27 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {storage} from "./firebase/firebase";
 
-function App() {
+function UploadForm() {
+  const allInputs = {audioUrl: ''}
+  const [audioAsFile, setAudioAsFile] = useState('');
+  const [audioAsUrl, setAudioAsUrl] = useState(allInputs);
+
+  console.log(audioAsFile)
+  const handleAudioAsFile = (e) => {
+  const audio = e.target.files [0]
+  setAudioAsFile (audioFile => (audio))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div className="App">
+    <form>
+      <input type="file" onChange={handleAudioAsFile}></input>
+    </form>
+  </div>
+);
 }
 
 
-export default App;
+
+export default UploadForm;
--- musicbox/src/MediaPool.js
+++ musicbox/src/MediaPool.js
@@ -1,17 +1,166 @@
-import React from 'react';
+import React, {Component} from "react";
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ListGroup from 'react-bootstrap/ListGroup';
+import {storage} from "./firebase/firebase";
 
-function MediaPool() {
-    return (
-        <div className="App">
-            <div className="Title">
-                <h1>this should be the media pool area</h1>
-                <h4>your go to portal for bands and artists to store music files and collaborate in realtime 🎶</h4>
-                <Button variant="primary">Upload File</Button>{' '}
-            </div>
-        </div>
+
+// // function UploadForm() {
+// //     const allInputs = {audioUrl: ''}
+// //     const [audioAsFile, setAudioAsFile] = useState('');
+// //     const [audioAsUrl, setAudioAsUrl] = useState(allInputs);
+
+// //     console.log(audioAsFile)
+// //     const handleAudioAsFile = (e) => {
+// //         const audio = e.target.files [0]
+// //         setAudioAsFile (audioFile => (audio))
+// //     }
+// //     return (
+// //     <div className="App">
+// //       <form>
+// //         <input type="file" onChange={handleAudioAsFile}></input>
+// //       </form>
+// //     </div>
+// //   );
+// // }
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import firebase from "firebase";
+// import FileUploader from "react-firebase-file-uploader";
+
+// class MediaPool extends React.Component {
+//     state = {
+//         filenames: [],
+//         downloadURLs: [],
+//         isUploading: false,
+//         uploadProgress: 0
+//     };
+
+//     handleUploadStart = () =>
+//     this.setState ({
+//         isUploading: true,
+//         uploadProgress: 0
+//     });
+
+//     handleProgress = progress =>
+//     this.setState ({
+//         uploadProgress: progress
+//     });
+
+//     handleUploadError = error =>
+// }
+
+// function MediaPool() {
+//     return (
+//         <div className="App">
+//             <div className="Title">
+//                 <Container>
+//                     <h1>Media Pool</h1>
+//                     <Row>
+//                         <Col md = "auto">Upload media here:</Col>
+//                         <Button variant="primary" onClick= {UploadForm}>Upload File</Button>{' '}
+//                         <Col>
+//                             <ListGroup></ListGroup>
+
+//                         </Col>
+//                     </Row>
+//                 </Container>
+//             </div>
+//         </div>
+//     );
+// }
+
+// // export default MediaPool;
+
+class AudioUpload extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      audio: null,
+      url: "",
+      progress: 0
+    };
+  }
+
+  handleChange = e => {
+    if (e.target.files[0]) {
+      const audio = e.target.files[0];
+      this.setState(() => ({ audio }));
+    }
+  };
+
+  handleUpload = () => {
+    const { audio } = this.state;
+    const uploadTask = storage.ref(`audio/${audio.name}`).put(audio);
+    uploadTask.on(
+      "state_changed",
+      snapshot => {
+        // progress function ...
+        const progress = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        this.setState({ progress });
+      },
+      error => {
+        // Error function ...
+        console.log(error);
+      },
+      () => {
+        // complete function ...
+        storage
+          .ref("audio")
+          .child(audio.name)
+          .getDownloadURL()
+          .then(url => {
+            this.setState({ url });
+          });
+      }
     );
+  };
+  render() {
+    return (
+      <div className="center">
+          <br/>
+          <h2 className="green-text">React Firebase Audio Uploader</h2>
+          <br/>
+          <br/>
+        <div className="row">
+          <progress value={this.state.progress} max="100" className="progress" />
+        </div>
+        <br />
+        <br />
+        <br />
+        <div className="file-field input-field">
+          <div className="btn">
+            <span>File</span>
+            <input type="file" onChange={this.handleChange} />
+          </div>
+          <div className="file-path-wrapper">
+            <input className="file-path validate" type="text" />
+          </div>
+        </div>
+        <button
+          onClick={this.handleUpload}
+          className="waves-effect waves-light btn"
+        >
+          Upload
+        </button>
+        <br />
+        <br />
+        <img
+          src={this.state.url || "https://via.placeholder.com/400x300"}
+          alt="Uploaded Audio"
+          height="300"
+          width="400"
+        />
+      </div>
+    );
+  }
 }
 
+export default AudioUpload;
 
-export default MediaPool;
+
