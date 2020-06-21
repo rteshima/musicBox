86ba73bf437d8963daa1b0cdc37be0427d90a657
ryanteshima <rteshim1@jhu.edu>
2020-06-20 20:26:25 -0700

    downloaded react and react-bootstrap for frontend elements

---
+++ musicbox/src/MediaPool.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+function MediaPool() {
+    return (
+        <div className="App">
+            <div className="Title">
+                <h1>this should be the media pool area</h1>
+                <h4>your go to portal for bands and artists to store music files and collaborate in realtime 🎶</h4>
+                <Button variant="primary">Upload File</Button>{' '}
+            </div>
+        </div>
+    );
+}
+
+
+export default MediaPool;
---
+++ musicbox/src/TitleText.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function TitleText() {
+    return (
+        <div className="App">
+            <div className="Title">
+                <h1>welcome to musicbox</h1>
+                <h4>your go to portal for bands and artists to store music files and collaborate in realtime 🎶</h4>
+            </div>
+        </div>
+    );
+}
+
+
+export default TitleText;
