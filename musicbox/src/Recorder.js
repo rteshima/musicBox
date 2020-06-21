e3814ca83d625afc3a2279aa947bb273b8d70864
ryanteshima <rteshim1@jhu.edu>
2020-06-20 22:58:19 -0700

    adding recorder frontend

---
+++ musicbox/src/Recorder.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './styles/Recorder.css';
+import redLight from './images/red.png';
+import grayLight from './images/gray.png';
+
+// class RecorderButton extends React.Component() {
+//     constructor(props) {
+//         super(props);
+//     }
+
+
+//     createRecorder() {
+//         return <Recorder name="Ryan" live={true} />;
+//     }
+
+//     render() {
+//         return (
+//             <button onClick={this.createRecorder}>connect device</button>
+//         );
+//     }
+// }
+
+function Recorder(props) {
+    return (
+        <div className="Recorder">
+            <Container>
+                <Row className="show-grid recorder-row">
+                    <Col sm={2} className="recorder-col">
+                        <RecLight live={props.live} />;
+                    </Col>
+                    <Col sm={2} className="recorder-col">{props.name}</Col>
+                    <Col sm={8} className="recorder-col">sm=8</Col>
+                </Row>
+            </Container>
+        </div>
+    );
+}
+
+function RecLight(props) {
+    if (props.live) {
+        return (<img className="Recording" src={redLight} />);
+    } else {
+        return (<img className="NotRecording" src={grayLight} />);
+    }
+}
+
+export default Recorder;
